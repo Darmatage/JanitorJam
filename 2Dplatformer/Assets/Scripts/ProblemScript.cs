0a9b4ba51f5a0cecf9a1e977247668c17405164c
bayley105 <79952159+bayley105@users.noreply.github.com>
2021-03-07 20:59:23 -0500

    Create ProblemScript.cs

---
+++ 2Dplatformer/Assets/Scripts/ProblemScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProblemScript : MonoBehaviour
+{
+    public bool solvedByMop;
+    public bool solvedByBroom;
+
+    public class problem
+    {
+        public bool solvedByMop;
+        public bool solvedByBroom;
+
+        public problem(bool mop, bool broom)
+        {
+            solvedByMop = mop;
+            solvedByBroom = broom;
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        problem problemInstance = new problem(solvedByMop, solvedByBroom);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
+
+
