af8d780696737fe154067ea8e4b0d2a432efcd74
bayley105 <79952159+bayley105@users.noreply.github.com>
2021-03-08 13:00:06 -0500

    Update ProblemScript.cs

--- 2Dplatformer/Assets/Scripts/ProblemScript.cs
+++ 2Dplatformer/Assets/Scripts/ProblemScript.cs
@@ -4,26 +4,13 @@ using UnityEngine;
 
 public class ProblemScript : MonoBehaviour
 {
-    public bool solvedByMop = false;
-    public bool solvedByBroom = false;
-
-    public class problem
-    {
-        public bool solvedByMop;
-        public bool solvedByBroom;
-
-        public problem(bool mop, bool broom)
-        {
-            solvedByMop = mop;
-            solvedByBroom = broom;
-        }
-    }
+    public bool solvedByMop;
+    public bool solvedByBroom;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        problem problemInstance = new problem(solvedByMop, solvedByBroom);
     }
 
     // Update is called once per frame
