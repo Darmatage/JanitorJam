343c6dc6304a35dbc544ea67d2377a873aff273b
bachngodinh <59807965+bachngodinh@users.noreply.github.com>
2021-03-07 19:28:54 -0500

    Bach push

---
+++ 2Dplatformer/Assets/Scripts/Character2DController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character2DController : MonoBehaviour
+{
+    public float MovementSpeed = 2;
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        var movement = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
+
+        var movementVertical = Input.GetAxis("Vertical");
+        transform.position += new Vector3(0, movementVertical, 0) * Time.deltaTime * MovementSpeed;
+    }
+}
